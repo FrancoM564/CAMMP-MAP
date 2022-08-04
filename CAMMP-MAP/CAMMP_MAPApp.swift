12918bf2c52fe1145b19dbfa688eb1855bbed3b0
Franco Marquez <20191218@aloe.ulima.edu.pe>
2022-08-04 13:36:28 -0500

    Initial Commit

---
+++ CAMMP-MAP/CAMMP_MAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CAMMP_MAPApp.swift
+//  CAMMP-MAP
+//
+//  Created by Franco Marquez on 4/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CAMMP_MAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
